12b12f18b2a1982098a588e4a2e1a371e16d284d
orangepek0e <georgejohnston13@hotmail.com>
2016-01-06 12:27:38 -0400

    facebook Auth finished

--- app/routes.js
+++ app/routes.js
@@ -66,6 +66,25 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+    // route for logging out
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
 
 };
 
---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '921400361300667', // your App ID
+        'clientSecret'  : 'bf311882249b31529804eea96e759f68', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'PYlvcfxyzr8rZBqQ7cTw8TLlO',
+        'consumerSecret'    : 'RJwwmxkFeboQgRYgvgbWjI2EtVq5cZpuYvGZ31u2Gb8o6t7ZtO',
+        'callbackURL'       : 'http://24.222.225.142:8080/auth/twitter/callback'
+    }
+
+};
